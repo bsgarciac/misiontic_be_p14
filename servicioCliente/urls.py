9ec3046ed328c92d85d6142be49e9fee10444c49
Brayan Garcia <brayan.garcia@puntopay.co>
2021-11-10 10:01:49 -0500

    Fixing serializer_class

---
+++ servicioCliente/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import SoporteListCreate, SoporteUpdateDelete, PQRListCreate, PQRUpdateDelete
+
+urlpatterns = [
+    path('soporte/', SoporteListCreate.as_view()),
+    path('soporte/<pk>/', SoporteUpdateDelete.as_view()),
+    path('pqrs/', PQRListCreate.as_view()),
+    path('pqr/<pk>/', PQRUpdateDelete.as_view()),
+]
--- servicioCliente/views.py
+++ servicioCliente/views.py
@@ -7,16 +7,16 @@ from .serializer import SoporteSerializer, PQRSerializer
 
 class SoporteListCreate(generics.ListCreateAPIView):
     queryset = Soporte.objects.all()
-    serializers_class = SoporteSerializer
+    serializer_class = SoporteSerializer
 
 class SoporteUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
     queryset = Soporte.objects.all()
-    serializers_class = SoporteSerializer
+    serializer_class = SoporteSerializer
 
 class PQRListCreate(generics.ListCreateAPIView):
     queryset = PQR.objects.all()
-    serializers_class = PQRSerializer
+    serializer_class = PQRSerializer
 
 class PQRUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
     queryset = PQR.objects.all()
-    serializers_class = PQRSerializer
+    serializer_class = PQRSerializer
